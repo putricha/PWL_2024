0956654cded6137d2bdc0044ab645aa20dc8b1f6
putricha_ <putrinorchasana@gmail.com>
2024-02-28 14:50:07 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route :: get ('/pesan', function(){
+    return 'Selamat Datang';
+});
+
+Route :: get ('/about',function(){
+    return '2241720036 => Putri Norchasana';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+return 'Halaman Artikel dengan ID ' . $id ;
+});
+
